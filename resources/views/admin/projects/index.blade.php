4719d4ea934fac5619cb51525d0170b66256c229
balbo97 <balbosauro1@gmail.com>
2024-02-23 13:49:17 +0800

    creazione index, show e layout admin creato e riempito di contenuto

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>I miei Progetti</h2>
+            </div>
+        </div>
+    </div>
+@endsection
