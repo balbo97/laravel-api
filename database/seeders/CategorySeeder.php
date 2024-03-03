ff609b70ef2dfc324541c3a03a45671d6e61ee6d
balbo97 <balbosauro1@gmail.com>
2024-03-03 18:41:42 +0800

    migration e seeding completate

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,8 +16,16 @@ class CategorySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+
+            $category = new Category();
+            
+            $category->name = $faker->word();
+            $category->slug = Str::slug($category->name, '-');
+
+            $category->save();
+        }
     }
 }
