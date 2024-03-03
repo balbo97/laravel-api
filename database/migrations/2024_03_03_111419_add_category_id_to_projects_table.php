d369d2489799c798ae17f9d82627b9e351471dc0
balbo97 <balbosauro1@gmail.com>
2024-03-03 19:47:57 +0800

    aggiunta colonna foreign key nella tabella

---
+++ database/migrations/2024_03_03_111419_add_category_id_to_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            // $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            // $table->foreign('category_id')->references('id')->on('category')->onDelete('set null');
+            $table->foreignId('category_id')->nullable()->after('id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+};
